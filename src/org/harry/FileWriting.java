b7318fdc9c380462120b632cd86af2a0110b379e
obrien1 <obrien1@PC13034.huntington-ed.org.uk>
2019-09-30 13:03:19 +0100

    added FileWriting.java

---
+++ src/org/harry/FileWriting.java
@@ -0,0 +1,16 @@
+package org.harry;
+
+import java.io.*;
+
+public class FileWriting {
+    public static void main(String[] args){
+     
+        try{
+            
+        }
+        catch(IOException e){
+        
+        }
+        
+    }
+}
