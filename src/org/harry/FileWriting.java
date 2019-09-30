62cd5292bd12db1227ca2855daedab65f642713c
harryob <harry@o-brien.me>
2019-09-30 13:27:41 +0100

    edited FileWriting.java

--- src/org/harry/FileWriting.java
+++ src/org/harry/FileWriting.java
@@ -6,10 +6,16 @@ public class FileWriting {
     public static void main(String[] args){
      
         try{
+            FileWriter file = new FileWriter("resources/writing.txt");
+            BufferedWriter buff = new BufferedWriter(file);
             
+            buff.write("This is a text file.");
+            buff.write("Here is another sentence.");
+            
+            buff.close();   
         }
-        catch(IOException e){
-        
+        catch(IOException e){   
+            System.out.println("The code did an IOException");
         }
         
     }
