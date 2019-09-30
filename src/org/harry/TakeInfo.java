159414fbe2b637715a1afdd08b43de019a85d9d2
harryob <harry@o-brien.me>
2019-09-30 13:37:14 +0100

    Added TakeInfo.java

---
+++ src/org/harry/TakeInfo.java
@@ -0,0 +1,36 @@
+package org.harry;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TakeInfo {
+    public static void main(String[] args) throws IOException{
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("What's your name?");
+        String name = userInput.nextLine();
+        
+        System.out.println("What's your age?");
+        String age = userInput.nextLine();
+        
+        System.out.println("What's your height?");
+        String height = userInput.nextLine();
+        
+        String data = name + ":" + age + ":" + height;
+        
+        try{
+            FileWriter file = new FileWriter("resources/takeinput.txt");
+            BufferedWriter buff = new BufferedWrite(file);
+            
+            buff.write(data + "\n");
+            
+            buff.close();
+        }
+        catch(IOException e){
+            System.out.println("IOException Error.");
+        }
+        
+    }
+    
+}
